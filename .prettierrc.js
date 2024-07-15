903f114641063e2ba16318f48e29a749abc964af
Andrea Paracino <andre.paracino@gmail.com>
2024-07-15 12:40:16 +0200

    feat: add config files & gitignore

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  printWidth: 100,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  endOfLine: 'lf',
+  tailwindConfig: './tailwind.config.js',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json'
+      }
+    }
+  ],
+  plugins: [require('prettier-plugin-tailwindcss')]
+}
